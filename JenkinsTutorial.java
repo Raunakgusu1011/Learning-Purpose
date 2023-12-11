a2969aec7e0e435e294f536ae98b24be1ec1a74e
Tabrez <tab.aec786@gmail.com>
2023-12-11 20:08:07 +0530

    third commit

--- JenkinsTutorial.java
+++ JenkinsTutorial.java
@@ -1,5 +1,6 @@
 public class JenkinsTutorial {
     public static void main(String[] args) {
         System.out.println("Jenkins First tutorial......");
+        System.out.println("Jenkins will auto detect it and build it....");
     }
 }
